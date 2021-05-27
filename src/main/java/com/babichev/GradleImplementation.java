79d474fe179e973ed498c9be6188dd14a5f27623
dasWarder <Al.94_94@mail.ru>
2021-05-27 11:43:07 +0300

    Add gradle basic structure and update build file

---
+++ src/main/java/com/babichev/GradleImplementation.java
@@ -0,0 +1,9 @@
+package com.babichev;
+
+public class GradleImplementation {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.getGreeting());
+    }
+}
---
+++ src/main/java/com/babichev/Greeter.java
@@ -0,0 +1,8 @@
+package com.babichev;
+
+public class Greeter {
+
+    public String getGreeting() {
+        return "Hello, Gradle from Greeter!";
+    }
+}
